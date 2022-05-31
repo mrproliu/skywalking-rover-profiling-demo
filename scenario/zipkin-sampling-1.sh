fc451fb0c680a4b40fb27ab1d7b97a31723a7f8f
Mrproliu <741550557@qq.com>
2022-05-31 22:12:00 +0800

    init

--- scenario/zipkin-sampling-1.sh
+++ scenario/zipkin-sampling-1.sh
@@ -4,6 +4,6 @@ istioctl install -y --set profile=demo \
   --set 'meshConfig.defaultConfig.tracing.sampling=1.0' \
 	--set 'meshConfig.defaultConfig.proxyStatsMatcher.inclusionRegexps[0]=.*upstream_rq_total.*'
 
-#sh $curdir/../resources/skywalking/install.sh
+sh $curdir/../resources/skywalking/install.sh
 sh $curdir/../resources/app/install.sh
 sh $curdir/../resources/prometheus/install.sh
