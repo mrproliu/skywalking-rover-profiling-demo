aa232c8334066e9f179fb22ac0e472670a537929
Mrproliu <741550557@qq.com>
2022-06-13 17:10:20 +0800

    Update skywalking install script

--- resources/skywalking/install.sh
+++ resources/skywalking/install.sh
@@ -2,20 +2,11 @@ rm -rf skywalking-kubernetes && git clone https://github.com/apache/skywalking-k
 cd skywalking-kubernetes/chart
 helm dep up skywalking
 helm -n istio-system install skywalking skywalking \
-                       --set fullnameOverride=skywalking \
-                       --set elasticsearch.replicas=2 \
-                       --set elasticsearch.minimumMasterNodes=1 \
                        --set elasticsearch.imageTag=7.5.1 \
-                       --set elasticsearch.nodeSelector.role=skywalking \
-                       --set oap.replicas=2 \
-                       --set oap.nodeSelector.role=skywalking \
-                       --set ui.image.repository=gcr.io/hanliu-20211231-155028/ui \
-                       --set ui.image.tag=latest \
-                       --set ui.image.pullPolicy=Always \
-                       --set ui.nodeSelector.role=skywalking \
-                       --set oap.image.tag=latest \
-                       --set oap.image.pullPolicy=Always \
-                       --set oap.image.repository=gcr.io/hanliu-20211231-155028/oap \
+                       --set oap.replicas=1 \
+                       --set elasticsearch.replicas=1 \
+                       --set ui.image.tag=9.1.0 \
+                       --set oap.image.tag=9.1.0 \
                        --set oap.storageType=elasticsearch
 
 curdir=$(cd $(dirname $0); pwd)
